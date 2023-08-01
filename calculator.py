2f1fadae43229b49860fee8879c2583140224ac2
Houman Hafez Alghoran <houman.hafezalghoran@gmail.com>
2023-08-02 01:59:27 +0200

    - Changes to the validation of the calculator

--- calculator.py
+++ calculator.py
@@ -4,44 +4,34 @@
     
 from flask import Flask, render_template, request
 
-#initiation of a flask class
 app = Flask(__name__)
-# Serve static files
 app.static_folder = 'static'
 
-#handles GET and POST requests with the following line
 @app.route('/', methods=['GET', 'POST'])
-
-# A function to 
 def calculator():
-    #initiation of two variables with empty strings
     expression = ''
     result = ''
+    error_message = ''
 
-    # A condition where if the request is a POST, it will handle it.
     if request.method == 'POST':
-        #button and expression values get added to the variables
         expression = request.form['expression']
         button = request.form['button']
 
-        #if the Clear button is pressed, it will clear the expression and result
         if button == 'C':
             expression = ''
             result = ''
-        #if the equals to button is pressed, it will show the result
         elif button == '=':
-            # evaluate and if it didn't work, give the value "Error" to the variable
-            try:
-                result = eval(expression)
-            except:
-                result = 'Error'
-        #If any other buttons are pressed, just add the value to the expression the user has
+            if all(char.isdigit() or char in '+-*/.' for char in expression):  # Check if the expression contains only digits and valid operators.
+                try:
+                    result = eval(expression)
+                except:
+                    result = 'Error'
+            else:
+                error_message = 'Invalid expression!'
         else:
             expression += button
 
-    # This line renders the template index.html with all the updated values etc.
-    return render_template('index.html', expression=expression, result=result)
+    return render_template('index.html', expression=expression, result=result, error_message=error_message)
 
-# Multiprocessing runs the class
 if __name__ == '__main__':
     app.run(debug=True)
